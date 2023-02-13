352bf63f7bcb17c7823431346cbcd334c7ac9c27
Juniooor <juniormodern@hotmail.com>
2023-02-13 20:39:08 -0300

    configurando script

--- valoresCripto.py
+++ valoresCripto.py
@@ -5,5 +5,17 @@ import requests
 requisicao = requests.get("http://economia.awesomeapi.com.br/json/last/ETH-BRL")
 
 requisicao_dict = requisicao.json()
-print(f'valor atual = {requisicao_dict["ETHBRL"]["bid"]}')
-print(f'horario da consulta = {requisicao_dict["ETHBRL"]["create_date"]}')
+
+valor_eth = requisicao_dict["ETHBRL"]["bid"]
+horario = requisicao_dict["ETHBRL"]["create_date"]
+
+print(type(valor_eth))
+
+if valor_eth < '7826':
+    print('menor', valor_eth )
+elif valor_eth > '8200':
+    print('maior', valor_eth)
+    
+    
+print(f'valor atual = {valor_eth}')
+print(f'horario da consulta = {horario}')
